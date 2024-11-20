b2efb7c58612aa736bc35a7399995a482640faec
nawelGl <ghazal.nawel@gmail.com>
2024-11-20 14:40:07 +0100

    reinstallation de node_modules (OK en local now)

--- proto-front/src/constants/back.js
+++ proto-front/src/constants/back.js
@@ -1,5 +1,5 @@
 
-export const LOCAL_HOST = 'http://172.31.249.34:8080';
+export const LOCAL_HOST = 'http://localhost:8080';
 export const LOCAL_HOST_SAMPLE = LOCAL_HOST + '/sample/';
 export const GET_SAMPLES = LOCAL_HOST_SAMPLE + 'all';
 export const UPDATE_SAMPLES = LOCAL_HOST_SAMPLE + 'update';
