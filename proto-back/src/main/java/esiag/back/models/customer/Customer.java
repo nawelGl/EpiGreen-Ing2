666c3379ecf2c14606b8b73437f700b296f2a7f5
nawelGl <ghazal.nawel@gmail.com>
2024-12-13 15:49:45 +0100

    Ajout de la classe Customer.java (back, model)

---
+++ proto-back/src/main/java/esiag/back/models/customer/Customer.java
@@ -0,0 +1,30 @@
+@Entity
+@Data
+@Table(name = "Customer")
+public class Customer{
+    @Id
+    @Column(name="CustomerId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int customerId;
+
+    @Column(name = "Lastname")
+    private String lastname;
+
+    @Column(name = "Firstname")
+    private String fristname;
+
+    @Column(name = "Birthdate")
+    private Date birthdate;
+
+    @Column(name = "StreetNumber")
+    private int streetNumber;
+
+    @Column(name = "Street")
+    private String street;
+
+    @Column(name = "ZipCode")
+    private int zipCode;
+
+    @Column(name = "City")
+    private String city;
+}
