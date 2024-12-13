25a4b6198003e0b15c0b2a2a89dc344343188fae
nawelGl <ghazal.nawel@gmail.com>
2024-12-13 21:46:21 +0100

    Ajout d'une deuxième méthode test calcul distance et appel dans main du back

--- proto-back/src/main/java/esiag/back/EsiagBackApplication.java
+++ proto-back/src/main/java/esiag/back/EsiagBackApplication.java
@@ -1,5 +1,7 @@
 package esiag.back;
 
+import esiag.back.controllers.httpRequests.DistanceRequest;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EsiagBackApplication {
 
 	public static void main(String[] args) {
+
+		//test une requete HTTP à l'api : OK
+		DistanceRequest.getDistanceFromApiTest(13.397634,52.529407,13.428555,52.593219);
+
 		SpringApplication.run(EsiagBackApplication.class, args);
 	}
 }
--- proto-back/src/main/java/esiag/back/controllers/httpRquests/DistanceRequest.java
+++ proto-back/src/main/java/esiag/back/controllers/httpRquests/DistanceRequest.java
@@ -8,7 +8,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class DistanceRequest{
-    public float getDistanceFromApi(Customer customer, Store store){
+    public static float getDistanceFromApi(Customer customer, Store store){
         double customerLatitude = customer.getY();
         double customerLongitude = customer.getX();
         double storeLatitude = store.getY();
@@ -19,15 +19,17 @@ public class DistanceRequest{
                 .uri(URI.create("https://api.jawg.io/routing/route/v1/car/" + customerLatitude + "," + customerLongitude + ";" + storeLatitude + "," + storeLongitude +"?overview=false&access-token=FUxkMYNp2rg3hG6JZc11POCUWUdzyLX8AXzrtcl1gilV2XMvf0BtFEOoPTtL8ZCJ"))
                 .build();
 
-//        HttpResponse<float> response;
-//        try {
-//            response = client.send(request, HttpResponse.BodyHandlers.ofFloat());
-//            villesEphemeride = mapper.readValue(response.body(), VillesEphemeride.class);
-//        } catch (Exception e) {
-//            System.out.println("Erreur lors de la requête HTTP : " + e.getMessage());
-//        }
-//
-//
+        HttpResponse<String> response;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            //villesEphemeride = mapper.readValue(response.body(), VillesEphemeride.class);
+            System.out.println("Réponse HTTP : " + response);
+        } catch (Exception e) {
+            System.out.println("Erreur lors de la requête HTTP : " + e.getMessage());
+        }
+
+
+
 //        HttpResponse<String> response;
 //        ObjectMapper mapper = new ObjectMapper();
 //        VillesEphemeride villesEphemeride = new VillesEphemeride();
@@ -37,6 +39,28 @@ public class DistanceRequest{
 //        } catch (Exception e) {
 //            System.out.println("Erreur lors de la requête HTTP : " + e.getMessage());
 //        }
+
+        return 0;
+    }
+
+    public static float getDistanceFromApiTest(double customerLatitude, double customerLongitude, double storeLatitude, double storeLongitude){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.jawg.io/routing/route/v1/car/" + customerLatitude + "," + customerLongitude + ";" + storeLatitude + "," + storeLongitude +"?overview=false&access-token=FUxkMYNp2rg3hG6JZc11POCUWUdzyLX8AXzrtcl1gilV2XMvf0BtFEOoPTtL8ZCJ"))
+                .build();
+
+        HttpResponse<String> response;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            //villesEphemeride = mapper.readValue(response.body(), VillesEphemeride.class);
+            System.out.println("Réponse HTTP : " + response);
+        } catch (Exception e) {
+            System.out.println("Erreur lors de la requête HTTP : " + e.getMessage());
+        }
+        //But pour l'instant : juste voir si on réussit à récupérer un print de la réponse
+        //Si oui, check comment récupérer la distance en elle même
+            //Solution 1 : mapper l'objet renvoyé
+            //Solution 2 : voir comment récupérer la réponse uniquement (solution optimale)
         return 0;
     }
 }
