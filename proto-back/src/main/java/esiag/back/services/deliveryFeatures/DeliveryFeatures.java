ca858783894d951a20afc1b4bad1d7306141791d
nawelGl <ghazal.nawel@gmail.com>
2024-12-13 15:11:30 +0100

    Ajout d'une classe deliveryFeatures pour les méthodes statiques concernant la livraison

---
+++ proto-back/src/main/java/esiag/back/services/deliveryFeatures/DeliveryFeatures.java
@@ -0,0 +1,4 @@
+public class DeliveryFeatures{
+    //Classe statique qui va permettre d'implémenter les méthodes relatives à la livraison.
+    //Méthode permetant de calculer la distance entre deux adresses : besoin d'une API
+}
