eb29cf490752c0c3d96967f924013da67b36ea9f
nawelGl <ghazal.nawel@gmail.com>
2024-12-21 23:18:40 +0100

    deplacement pacakge httpRequest de controlleurs à services

--- proto-back/src/main/java/esiag/back/services/deliveryFeatures/DeliveryFeatures.java
+++
@@ -1,4 +0,0 @@
-public class DeliveryFeatures{
-    //Classe statique qui va permettre d'implémenter les méthodes relatives à la livraison.
-    //Méthode permetant de calculer la distance entre deux adresses : besoin d'une API
-}
---
+++ proto-back/src/main/java/esiag/back/services/httpRquests/DistanceRequest.java
@@ -0,0 +1,74 @@
+package esiag.back.services.httpRequests;
+import esiag.back.models.customer.Customer;
+import esiag.back.models.store.Store;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DistanceRequest{
+    protected static Logger logger = LogManager.getLogger(DistanceRequest.class);
+    public static float getDistanceFromApi(Customer customer, Store store){
+        double customerLatitude = customer.getLatitude();
+        double customerLongitude = customer.getLongitude();
+        double storeLatitude = store.getLatitude();
+        double storeLongitude = store.getLongitude();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.jawg.io/routing/route/v1/car/" + customerLatitude + "," + customerLongitude + ";" + storeLatitude + "," + storeLongitude +"?overview=false&access-token=FUxkMYNp2rg3hG6JZc11POCUWUdzyLX8AXzrtcl1gilV2XMvf0BtFEOoPTtL8ZCJ"))
+                .build();
+
+        HttpResponse<String> response;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            logger.info("Réponse HTTP brute : " + response.body());
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(response.body());
+            float distance = (float) rootNode.get("routes").get(0).get("distance").asDouble();
+
+            logger.info("TEST LOGGER");
+            logger.info("Distance extraite : " + distance);
+            return distance;
+
+        } catch (Exception e) {
+            logger.warn("Erreur lors de la requête HTTP : " + e.getMessage());
+            return -1;
+        }
+    }
+
+
+
+    public static float getDistanceFromApiTest(double customerLatitude, double customerLongitude, double storeLatitude, double storeLongitude){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.jawg.io/routing/route/v1/car/" + customerLatitude + "," + customerLongitude + ";" + storeLatitude + "," + storeLongitude +"?overview=false&access-token=FUxkMYNp2rg3hG6JZc11POCUWUdzyLX8AXzrtcl1gilV2XMvf0BtFEOoPTtL8ZCJ"))
+                .build();
+
+        HttpResponse<String> response;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            logger.info("Réponse HTTP brute : " + response.body());
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(response.body());
+            float distance = (float) rootNode.get("routes").get(0).get("distance").asDouble();
+
+            logger.info("TEST LOGGER");
+            logger.info("Distance extraite : " + distance);
+            return distance;
+
+        } catch (Exception e) {
+            logger.warn("Erreur lors de la requête HTTP : " + e.getMessage());
+            return -1;
+        }
+    }
+}
