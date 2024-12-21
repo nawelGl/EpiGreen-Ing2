44e82d9d9e922b678ebe43506dd26698de7ed99f
nawelGl <ghazal.nawel@gmail.com>
2024-12-21 18:16:52 +0100

    Modification Customer.js pour afficher les données de la BDD

--- proto-front/src/components/Customer.js
+++ proto-front/src/components/Customer.js
@@ -21,23 +21,25 @@ export default function Customer() {
 
     return (
         <div className="container text-center">
-            <h4 className="mx-2">Customer List</h4>
+            <h4 className="mx-2">Liste des clients</h4>
             <div className="row">
                 <table className="table table-sm table-bordered table-hover">
                     <thead>
                     <tr>
                         <th scope="col">Id</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Prénom</th>
+                        <th scope="col">Adresse</th>
                     </tr>
                     </thead>
                     <tbody className="table-group-divider">
                     {
                         customers.map((customer, index) => (
                             <tr key={index}>
-                                <th scope="row">{customer.id}</th>
-                                <td>{customer.name}</td>
-                                <td>{customer.email}</td>
+                                <th scope="row">{customer.customerId}</th>
+                                <td>{customer.lastname}</td>
+                                <td>{customer.firstname}</td>
+                                <td>{customer.streetNumber + " " + customer.street + ", " + customer.zipCode + " " + customer.city}</td>
                             </tr>
                         ))
                     }
