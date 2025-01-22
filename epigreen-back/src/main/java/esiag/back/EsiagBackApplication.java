2521541d5dd336432843bc4ae2835c6aa508c892
seulgi <seulgi-shin@etu.u-pec.fr>
2025-01-22 11:30:03 +0100

    Revert "Merge branch 'dev' into seulgi"
    This reverts commit 0196bd5fe5ed5654785acf6d152c3bbd618b3cba, reversing
    changes made to 156d79b60b197fe2c59d9d6787f3949e288d798c.

--- epigreen-back/src/main/java/esiag/back/EsiagBackApplication.java
+++ epigreen-back/src/main/java/esiag/back/EsiagBackApplication.java
@@ -1,11 +1,11 @@
 package esiag.back;
 
-import esiag.back.services.httpRequests.DistanceRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EsiagBackApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(EsiagBackApplication.class, args);
 	}
--- epigreen-front/src/components/Router.js
+++ epigreen-front/src/components/Router.js
@@ -2,14 +2,10 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import App from "./App";
 import Sample from "./Sample";
-import DemoLivraison from "./DemoLivraison";
 import Navbar from "./Navbar";
 import NotFound from "./NotFound";
 import Search from "./Search";
 import Similarity from "./Similarity";
-import Customer from "./Customer";
-import Store from "./Store";
-
 
 export default function Router () {
     return (
@@ -19,9 +15,6 @@ export default function Router () {
                 <Routes>
                     <Route path="/" element={<App />}/>
                     <Route path="/sample" element={<Sample />}/>
-                    <Route path="/customer" element={<Customer />}/>
-                    <Route path="/store" element={<Store />}/>
-                    <Route path="/demoLivraison" element={<DemoLivraison />}/>
                     <Route path="*" element={<NotFound />}/>
                     <Route path="/search" element={<Search />}/>
                     <Route path="/similarity" element={<Similarity />}/>
